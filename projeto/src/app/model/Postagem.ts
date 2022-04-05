5844d1c8fe6eb57d6cf76ec7e964839005e2d823
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-04-05 15:46:17 -0300

    @RiqueMendes [task: post]

--- projeto/src/app/model/Postagem.ts
+++ projeto/src/app/model/Postagem.ts
@@ -1,3 +1,4 @@
+import { Tema } from "./Tema"
 import { User } from "./User"
 
 export class Postagem{
@@ -5,5 +6,7 @@ export class Postagem{
     public titulo: string
     public texto: string
     public data: Date
+    public usuario: User
+    public tema: Tema
     
 }
